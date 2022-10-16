66082ad243804a0725cd1aae0d765257d9e76ff7
Lucas Mansoldo <lucasmansoldo@Mac-mini-de-Lucas.local>
2022-10-16 16:47:23 -0300

    feat: new dropdown menu

---
+++ src/lib/components/dropdown-menu/dropdown-item.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let icon: string | undefined = undefined
+    export let label: string | undefined = undefined
+</script>
+
+<li on:click>
+    <span class={icon}><slot />{label || ""}</span>
+</li>
---
+++ src/lib/components/dropdown-menu/dropdown-menu.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let open: false
+</script>
+
+<div class="flex flex-row items-center shadow-sm shadow-additional-500 justify-center bg-secondary-50 min-h-[4.375rem] drop-shadow-sm px-5 relative left-0 right-0">
+	{#if open}
+		<ul>
+			<slot />
+		</ul>
+	{/if}
+</div>
